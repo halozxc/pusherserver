a452c7ddae7dcdba60ddf4d51ac11333d9fcf0b6
halozxc <halozxc>
2022-02-28 12:53:23 +0800

    mysql update

--- pusher.sql
+++ pusher.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
 --
--- Host: 192.168.1.5    Database: pusher
+-- Host: 127.0.0.1    Database: pusher
 -- ------------------------------------------------------
--- Server version	8.0.23
+-- Server version	8.0.19
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `contact`
+--
+
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `contact` (
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `user_1` int NOT NULL,
+                           `user_2` int NOT NULL,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `contact_id_uindex` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `contact`
+--
+
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `file`
 --
 
@@ -23,13 +48,13 @@ DROP TABLE IF EXISTS `file`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `file` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` int DEFAULT NULL,
-  `belong_user_id` int DEFAULT NULL,
-  `time` timestamp NULL DEFAULT NULL,
-  `exist` tinyint(1) DEFAULT NULL,
-  `real_uuid` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `name` int DEFAULT NULL,
+                        `belong_user_id` int DEFAULT NULL,
+                        `time` timestamp NULL DEFAULT NULL,
+                        `exist` tinyint(1) DEFAULT NULL,
+                        `real_uuid` varchar(50) DEFAULT NULL,
+                        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -50,8 +75,8 @@ DROP TABLE IF EXISTS `file_group`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `file_group` (
-  `file_id` int DEFAULT NULL,
-  `group_id` int DEFAULT NULL
+                              `file_id` int DEFAULT NULL,
+                              `group_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -72,8 +97,8 @@ DROP TABLE IF EXISTS `file_matter`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `file_matter` (
-  `matter_id` int DEFAULT NULL,
-  `file_id` int DEFAULT NULL
+                               `matter_id` int DEFAULT NULL,
+                               `file_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -94,9 +119,9 @@ DROP TABLE IF EXISTS `group_fan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `group_fan` (
-  `group_id` int DEFAULT NULL,
-  `user_id` int DEFAULT NULL,
-  `display_name` varchar(30) DEFAULT NULL
+                             `group_id` int DEFAULT NULL,
+                             `user_id` int DEFAULT NULL,
+                             `display_name` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -106,7 +131,7 @@ CREATE TABLE `group_fan` (
 
 LOCK TABLES `group_fan` WRITE;
 /*!40000 ALTER TABLE `group_fan` DISABLE KEYS */;
-INSERT INTO `group_fan` VALUES (7,1,'test'),(8,1,NULL),(8,2,'咸鱼群员');
+INSERT INTO `group_fan` (`group_id`, `user_id`, `display_name`) VALUES (7,1,'test'),(8,1,NULL),(8,2,'咸鱼群员');
 /*!40000 ALTER TABLE `group_fan` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -118,8 +143,8 @@ DROP TABLE IF EXISTS `group_manager`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `group_manager` (
-  `group_id` int DEFAULT NULL,
-  `manager_user_id` int DEFAULT NULL
+                                 `group_id` int DEFAULT NULL,
+                                 `manager_user_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -140,10 +165,10 @@ DROP TABLE IF EXISTS `group_matter`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `group_matter` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `group_id` int DEFAULT NULL,
-  `matter_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                                `id` int NOT NULL AUTO_INCREMENT,
+                                `group_id` int DEFAULT NULL,
+                                `matter_id` int DEFAULT NULL,
+                                PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -153,7 +178,7 @@ CREATE TABLE `group_matter` (
 
 LOCK TABLES `group_matter` WRITE;
 /*!40000 ALTER TABLE `group_matter` DISABLE KEYS */;
-INSERT INTO `group_matter` VALUES (2,7,17),(3,7,18);
+INSERT INTO `group_matter` (`id`, `group_id`, `matter_id`) VALUES (2,7,17),(3,7,18);
 /*!40000 ALTER TABLE `group_matter` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,10 +190,10 @@ DROP TABLE IF EXISTS `group_pusher`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `group_pusher` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `groupname` varchar(50) DEFAULT NULL,
-  `creator_user_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                                `id` int NOT NULL AUTO_INCREMENT,
+                                `groupname` varchar(50) DEFAULT NULL,
+                                `creator_user_id` int DEFAULT NULL,
+                                PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -178,7 +203,7 @@ CREATE TABLE `group_pusher` (
 
 LOCK TABLES `group_pusher` WRITE;
 /*!40000 ALTER TABLE `group_pusher` DISABLE KEYS */;
-INSERT INTO `group_pusher` VALUES (7,'测试群',1),(8,'新群名',1);
+INSERT INTO `group_pusher` (`id`, `groupname`, `creator_user_id`) VALUES (7,'测试群',1),(8,'新群名',1);
 /*!40000 ALTER TABLE `group_pusher` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -190,19 +215,19 @@ DROP TABLE IF EXISTS `matter`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `matter` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `headline` varchar(100) DEFAULT NULL,
-  `startdate` timestamp NULL DEFAULT NULL,
-  `enddate` timestamp NULL DEFAULT NULL,
-  `location` varchar(100) DEFAULT NULL,
-  `isergency` tinyint(1) DEFAULT NULL,
-  `body` varchar(800) DEFAULT NULL,
-  `has_file` tinyint(1) DEFAULT NULL,
-  `message_id` int DEFAULT NULL,
-  `manager_user_id` int DEFAULT NULL,
-  `repeat` enum('NONE','DAY','WEEK','MONTH','YEAR') DEFAULT NULL,
-  `publish_date` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                          `id` int NOT NULL AUTO_INCREMENT,
+                          `headline` varchar(100) DEFAULT NULL,
+                          `startdate` timestamp NULL DEFAULT NULL,
+                          `enddate` timestamp NULL DEFAULT NULL,
+                          `location` varchar(100) DEFAULT NULL,
+                          `isergency` tinyint(1) DEFAULT NULL,
+                          `body` varchar(800) DEFAULT NULL,
+                          `has_file` tinyint(1) DEFAULT NULL,
+                          `message_id` int DEFAULT NULL,
+                          `manager_user_id` int DEFAULT NULL,
+                          `repeat` enum('NONE','DAY','WEEK','MONTH','YEAR') DEFAULT NULL,
+                          `publish_date` timestamp NULL DEFAULT NULL,
+                          PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -212,7 +237,7 @@ CREATE TABLE `matter` (
 
 LOCK TABLES `matter` WRITE;
 /*!40000 ALTER TABLE `matter` DISABLE KEYS */;
-INSERT INTO `matter` VALUES (17,'上班','2021-07-18 01:34:33','2021-07-18 01:34:33','宿舍',1,'到底上不上',NULL,NULL,1,NULL,'2021-07-19 01:31:21'),(18,'上班','2021-07-18 01:34:33','2021-07-18 01:34:33','宿舍',1,'到底上不上',NULL,NULL,1,NULL,'2021-07-19 03:12:41');
+INSERT INTO `matter` (`id`, `headline`, `startdate`, `enddate`, `location`, `isergency`, `body`, `has_file`, `message_id`, `manager_user_id`, `repeat`, `publish_date`) VALUES (17,'上班','2021-07-18 01:34:33','2021-07-18 01:34:33','宿舍',1,'到底上不上',NULL,NULL,1,NULL,'2021-07-19 01:31:21'),(18,'上班','2021-07-18 01:34:33','2021-07-18 01:34:33','宿舍',1,'到底上不上',NULL,NULL,1,NULL,'2021-07-19 03:12:41');
 /*!40000 ALTER TABLE `matter` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -224,11 +249,11 @@ DROP TABLE IF EXISTS `matter_fan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `matter_fan` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `matter_id` int DEFAULT NULL,
-  `fan_id` int DEFAULT NULL,
-  `source_type` enum('USER','GROUP') DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                              `id` int NOT NULL AUTO_INCREMENT,
+                              `matter_id` int DEFAULT NULL,
+                              `fan_id` int DEFAULT NULL,
+                              `source_type` enum('USER','GROUP') DEFAULT NULL,
+                              PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -238,7 +263,7 @@ CREATE TABLE `matter_fan` (
 
 LOCK TABLES `matter_fan` WRITE;
 /*!40000 ALTER TABLE `matter_fan` DISABLE KEYS */;
-INSERT INTO `matter_fan` VALUES (1,17,1,NULL);
+INSERT INTO `matter_fan` (`id`, `matter_id`, `fan_id`, `source_type`) VALUES (1,17,1,NULL);
 /*!40000 ALTER TABLE `matter_fan` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -250,12 +275,12 @@ DROP TABLE IF EXISTS `message`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `message` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `sender` int DEFAULT NULL,
-  `destination_user_id` int DEFAULT NULL,
-  `destination_matter_id` int DEFAULT NULL,
-  `body` varchar(800) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `sender` int DEFAULT NULL,
+                           `destination_user_id` int DEFAULT NULL,
+                           `destination_matter_id` int DEFAULT NULL,
+                           `body` varchar(800) DEFAULT NULL,
+                           PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -276,15 +301,15 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `uname` varchar(100) DEFAULT NULL,
-  `nickname` varchar(30) DEFAULT NULL,
-  `upassword` varchar(100) DEFAULT NULL,
-  `utel` varchar(20) DEFAULT NULL,
-  `umail` varchar(30) DEFAULT NULL,
-  `salt` varchar(64) DEFAULT NULL,
-  `avatar_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`)
+                        `id` int NOT NULL AUTO_INCREMENT,
+                        `uname` varchar(100) DEFAULT NULL,
+                        `nickname` varchar(30) DEFAULT NULL,
+                        `upassword` varchar(100) DEFAULT NULL,
+                        `utel` varchar(20) DEFAULT NULL,
+                        `umail` varchar(30) DEFAULT NULL,
+                        `salt` varchar(64) DEFAULT NULL,
+                        `avatar_id` int DEFAULT NULL,
+                        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -294,7 +319,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'pusher_1','u1','7268d1079c7f386923eac0d6a1e808b9','13666665555',NULL,'77fb62b511334f5b945520a9d1128a1e',NULL),(2,'hmw','u2','29f352b00457ec5fd24925536b9165aa',NULL,NULL,'21fec5e6b8cd4951bce0f4e413ec5d99',NULL);
+INSERT INTO `user` (`id`, `uname`, `nickname`, `upassword`, `utel`, `umail`, `salt`, `avatar_id`) VALUES (1,'pusher_1','u1','7268d1079c7f386923eac0d6a1e808b9','13666665555',NULL,'77fb62b511334f5b945520a9d1128a1e',NULL),(2,'hmw','u2','29f352b00457ec5fd24925536b9165aa',NULL,NULL,'21fec5e6b8cd4951bce0f4e413ec5d99',NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -306,8 +331,8 @@ DROP TABLE IF EXISTS `user_fan`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user_fan` (
-  `user_id` int DEFAULT NULL,
-  `fan_user_id` int DEFAULT NULL
+                            `user_id` int DEFAULT NULL,
+                            `fan_user_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -329,4 +354,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-07-20  0:20:48
+-- Dump completed on 2022-02-28 11:45:40
