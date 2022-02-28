fdf93e4cb5a8317dbf8be6c2dcd1e6a659c2b1a6
halozxc <824927872@qq.com>
2022-03-01 00:34:15 +0800

    sleep

--- src/main/java/xyz/humilr/pusherserver/dao/ContactMapper.java
+++ src/main/java/xyz/humilr/pusherserver/dao/ContactMapper.java
@@ -1,10 +1,10 @@
 package xyz.humilr.pusherserver.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import xyz.humilr.pusherserver.pojo.module.Contact;
-import xyz.humilr.pusherserver.pojo.module.Message;
-
+@Mapper
 public interface ContactMapper extends  tk.mybatis.mapper.common.Mapper<Contact>{
     @Select("select * from contact where user_1 = #{user_1} and user_2 = #{user_2} or user_1 = #{user_2} and user_2 = #{user_1} order by id desc limit 1")
-    Contact vertifyContact(Integer user_1,Integer user_2);
+    boolean vertifyContact(Integer user_1,Integer user_2);
 }
--- src/main/java/xyz/humilr/pusherserver/service/MessageService.java
+++ src/main/java/xyz/humilr/pusherserver/service/MessageService.java
@@ -2,6 +2,7 @@ package xyz.humilr.pusherserver.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import xyz.humilr.pusherserver.dao.ContactMapper;
 import xyz.humilr.pusherserver.dao.GroupFanMapper;
 import xyz.humilr.pusherserver.dao.MessageMapper;
 import xyz.humilr.pusherserver.dao.UserMapper;
@@ -21,6 +22,8 @@ public class MessageService {
     UserService userService;
     @Autowired
     GroupService groupService;
+    @Autowired
+    ContactMapper contactMapper;
     public boolean publish(Message message){
 //        if(message.getSender() == null){
 //            return  false;
@@ -28,14 +31,19 @@ public class MessageService {
     if(message.getDestination_matter_id() != null){
         //send to the group
         if (!groupService.isUserInGroup(message.getSender(), message.getDestination_matter_id())) return false;
-        message.setId(null);
-        return messageMapper.insertSelective(message)>0;
+      return publishToDatabase(message);
     }
-else if(message.getDestination_user_id()!=null){
-
+    else if(message.getDestination_user_id()!=null){
+   if(!contactMapper.vertifyContact(message.getSender(),message.getDestination_user_id())){
+      return false;
+   }
+       return publishToDatabase(message);
     }
         return true;
     }
-
+    private boolean publishToDatabase(Message message){
+        message.setId(null);
+        return messageMapper.insertSelective(message)>0;
+    }
 
 }
