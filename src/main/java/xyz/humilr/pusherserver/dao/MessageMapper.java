8fd2486ed8149f23954e211736c06312d6efc14f
halozxc <40384526+halozxc@users.noreply.github.com>
2022-03-13 21:22:22 +0800

    ??

--- src/main/java/xyz/humilr/pusherserver/dao/MessageMapper.java
+++ src/main/java/xyz/humilr/pusherserver/dao/MessageMapper.java
@@ -1,9 +1,14 @@
 package xyz.humilr.pusherserver.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 import xyz.humilr.pusherserver.pojo.module.Matter;
 import xyz.humilr.pusherserver.pojo.module.Message;
 
+import java.util.List;
+
 @Mapper
 public interface MessageMapper extends tk.mybatis.mapper.common.Mapper<Message> {
+    @Select("select * from message where destination_user = #{uid} or exist ")
+    List<Message> queryMessage(String uid);
 }
